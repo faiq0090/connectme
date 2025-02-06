826de67511cc8b67e440ecfe40c6df10ada2b1f9
Faiq45301 <faiq45301@gmail.com>
2025-02-06 22:26:05 +0500

    LoginScreen

--- app/src/main/java/com/example/connectme/Signup.kt
+++ app/src/main/java/com/example/connectme/Signup.kt
@@ -1,27 +1,20 @@
 package com.example.connectme
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        var loginbtn = findViewById<Button>(R.id.loginButton)
-        loginbtn.setOnClickListener {
-            val intent = Intent(this, Signup::class.java)
-            startActivity(intent)
         }
     }
 }

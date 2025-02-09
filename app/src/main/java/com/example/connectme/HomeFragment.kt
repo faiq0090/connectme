5a7f86d4ca0351f84e486ff9ad419429f123dbb6
Faiq45301 <faiq45301@gmail.com>
2025-02-09 18:44:06 +0500

    send-btn-added-to-home

--- app/src/main/java/com/example/connectme/HomeFragment.kt
+++ app/src/main/java/com/example/connectme/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 
 
 class HomeFragment : Fragment() {
@@ -18,6 +20,13 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val imageView = view.findViewById<ImageView>(R.id.btnSend)
+        imageView.setOnClickListener {
+            Toast.makeText(context, "Image Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
